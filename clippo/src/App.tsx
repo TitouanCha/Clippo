ecbe18c8201c5acfaf61f6c1ccf866feaada8897
Titouan Chauché <titouanchauche123@gmail.com>
2026-05-28 14:53:53 +0200

    feat : add firts pop up

--- clippo/src/App.tsx
+++ clippo/src/App.tsx
@@ -1,50 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 import "./App.css";
+import image from "./assets/honk.png";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const handleMouseDown = () => {
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
   }
 
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      onMouseDown={(e) => {
+        e.currentTarget.style.cursor = "grabbing";
+        getCurrentWindow().startDragging();
+      }}
+      onMouseUp={(e) => {
+        e.currentTarget.style.cursor = "grab";
+      }}
+      className="w-[500px] h-[300px] rounded-3xl flex flex-row items-stretch text-white text-2xl backdrop-blur-xl"
+    >
+      <img
+        src={image}
+        alt="PNG"
+        className="max-w-[40%] max-h-full object-contain self-end rounded-xl pointer-events-none"
+      />
+      <div className="chat chat-start self-start">
+        <div className="chat-bubble">
+          It's over Anakin, I have the high ground!
+        </div>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+    </div>
   );
 }
 
